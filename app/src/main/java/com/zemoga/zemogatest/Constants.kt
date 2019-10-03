7407022c8e786f400673da561c4324c72d360740
YEPEJ010 <jaibercom@gmail.com>
2019-10-02 19:05:13 -0500

    initial project

---
+++ app/src/main/java/com/zemoga/zemogatest/Constants.kt
@@ -0,0 +1,5 @@
+package com.zemoga.zemogatest
+
+object Constants {
+    const val HOST_IP = "https://jsonplaceholder.typicode.com"
+}
---
+++ app/src/main/java/com/zemoga/zemogatest/api/ApiService.kt
@@ -0,0 +1,12 @@
+package com.zemoga.zemogatest.api
+
+import com.zemoga.zemogatest.model.Post
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("posts")
+    fun requestPosts() : Call<Post>
+
+}
