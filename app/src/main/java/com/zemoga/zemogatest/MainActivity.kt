078faba00bb6eaf33716bedf4f88cccb667de926
YEPEJ010 <jaibercom@gmail.com>
2019-10-11 14:30:01 -0500

    adding bottom navigation navigation + in progress

--- app/src/main/java/com/zemoga/zemogatest/MainActivity.kt
+++ app/src/main/java/com/zemoga/zemogatest/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
